042715a0b6fad6eec81dc8df9598794de2920e57
Lera <shorokhovalera10a@gmail.com>
2020-07-25 03:54:11 +0700

    Lol oru

--- IntForZadanie.py
+++ IntForZadanie.py
@@ -1,7 +1,21 @@
-a = open('BF.txt')
+ = open('BF.txt')
 c = [0,0,0,0,0,0,0,0,0,0]
 b = a.read()
 d = 0
 for i in b:
     if i == '+':
         c[d] += 1
+    elif i == '>':
+        d += 1
+    elif i == '<':
+        d -= 1
+    elif i == '-':
+        c[d] -= 1
+    elif i == '.':
+        print(c[d])
+    elif i == ',':
+        c[d] = input('Vvod')
+    elif i == '[':
+        if c[d] > 0:
+            d += 1
+        elsea
